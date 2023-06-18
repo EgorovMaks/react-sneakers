1c4dd541da12cee1019cdbabdab2af806c648fcc
EgorovMaks <funt.egorov@gmail.com>
2023-06-18 12:16:46 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 
 import Header from './components/Header';
 import Drawer from './components/Drawer';
-import Content from './components/Content';
+import Content from './pieces/Content';
 
 function App() {
   return (
-    <div className="wrap">
+    <div className="wrap"> 
       <Header/>
       <Drawer/>
       <Content/>
